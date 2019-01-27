2950886e2e687b95dc0f1340ab9a30c8e12c39d2
rbayu505 <restabayu.s@gmail.com>
2019-01-27 18:47:39 +0700

    revpenda dengan checking

--- app/src/main/java/id/restabayu/clientsatu/DetailActivity.java
+++ app/src/main/java/id/restabayu/clientsatu/DetailActivity.java
@@ -13,23 +13,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.lang.String;
+import java.util.List;
 
 import id.restabayu.clientsatu.Service.AlarmReciever;
 import id.restabayu.clientsatu.Service.Converter;
 import id.restabayu.clientsatu.Service.AlarmService;
 
 
-
 public class DetailActivity extends AppCompatActivity {
 
 
-    TextView namaTxt,tanggalTxt,waktuTxt,tempatTxt,deskripsiTxt,interval, tvInterval;
+    TextView namaTxt, tanggalTxt, waktuTxt, tempatTxt, deskripsiTxt, interval, tvInterval;
     AlarmManager alarmManager;
     private PendingIntent pendingIntent;
     String waktuAcara, tglAcara, vInterval, intervalTgl, intervalWaktu;
+    private Calendar calendar;
 
 
     @Override
@@ -46,7 +48,8 @@ public class DetailActivity extends AppCompatActivity {
         tvInterval = (TextView) findViewById(R.id.vInterval);
 
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-     //   ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
+        calendar = Calendar.getInstance();
+        //   ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
 
         //GET INTENT
         Intent i = this.getIntent();
@@ -82,45 +85,79 @@ public class DetailActivity extends AppCompatActivity {
         String time = waktuTxt.getText().toString();
         String dateTime = date + " " + time;
         Date txtAlarm = Converter.toDate(dateTime, "yyyy MM dd HH:mm");
+        Date current = calendar.getTime();
 
+
+        long setInterval = 0L;
         long secondsInMilli = 1000;
         long minutesInMilli = secondsInMilli * 60;
         long hoursInMilli = minutesInMilli * 60;
         long daysInMilli = hoursInMilli * 24;
+        List<Long> data = Arrays.asList(
+                daysInMilli,
+                hoursInMilli * 12,
+                hoursInMilli * 6,
+                hoursInMilli * 3,
+                hoursInMilli,
+                minutesInMilli * 30,
+                minutesInMilli * 3
+        );
+        long finalInterval;
+        int tipe;
 
-        long setInterval = 0L;
-        if(vInterval.equals("1")){
-            setInterval = minutesInMilli * 30;
-        }else if(vInterval.equals("2")){
-            setInterval = hoursInMilli;
-        }else if(vInterval.equals("3")){
-            setInterval = hoursInMilli * 3;
-        }else if(vInterval.equals("4")){
-            setInterval = hoursInMilli * 6;
-        }else if(vInterval.equals("5")){
-            setInterval = hoursInMilli * 12;
-        }else if(vInterval.equals("6")){
-            setInterval = daysInMilli;
-        }else {
-            setInterval = minutesInMilli * 30;
+        if (vInterval.equals("6")) {
+            setInterval = data.get(0);
+            tipe = 0;
+        } else if (vInterval.equals("5")) {
+            setInterval = data.get(1);
+            tipe = 1;
+        } else if (vInterval.equals("4")) {
+            setInterval = data.get(2);
+            tipe = 2;
+        } else if (vInterval.equals("3")) {
+            setInterval = data.get(3);
+            tipe = 3;
+        } else if (vInterval.equals("2")) {
+            setInterval = data.get(4);
+            tipe = 4;
+        } else if (vInterval.equals("1")) {
+            setInterval = data.get(5);
+            tipe = 5;
+        } else {
+            tipe = 6;
+            setInterval = minutesInMilli * 3;
         }
-
-
         long result = txtAlarm.getTime() - setInterval;
-        interval.setText(Converter.getDate(result, "dd/MM/yyyy HH:mm"));
+        if (current.getTime() > result) {
+            if(current.getTime() > txtAlarm.getTime() - data.get(5)){
+                finalInterval = txtAlarm.getTime() - data.get(6);
+                setAlarm(finalInterval);
+            }else {
+                long d = txtAlarm.getTime() - data.get(tipe+1);
+                finalInterval = d;
+                setAlarm(finalInterval);
+            }
+        } else {
+            finalInterval = result;
+            setAlarm(finalInterval);
+        }
+        //tgl_hasil_interva
+
+    }
+
+    private void setAlarm(long iVal) {
+        interval.setText(Converter.getDate(iVal, "dd/MM/yyyy HH:mm"));
         String dF = Converter.ConvertDate(interval.getText().toString(), "dd/MM/yyyy HH:mm", "yyyy MM dd HH:mm");
         Date resAlarm = Converter.toDate(dF, "yyyy MM dd HH:mm");
 
-
         Log.d("MyActivity", "Alarm On");
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(resAlarm);
-            Intent myIntent = new Intent(this, AlarmReciever.class);
-            pendingIntent = PendingIntent.getBroadcast(DetailActivity.this, 0, myIntent, 0);
-            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
 
-            Toast.makeText(getApplicationContext(), "Pengingat telah dibuat !", Toast.LENGTH_SHORT).show();
+        calendar.setTime(resAlarm);
+        Intent myIntent = new Intent(this, AlarmReciever.class);
+        pendingIntent = PendingIntent.getBroadcast(DetailActivity.this, 0, myIntent, 0);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
 
+        Toast.makeText(getApplicationContext(), "Pengingat telah dibuat !", Toast.LENGTH_SHORT).show();
     }
 
      /*   public void onToggleClicked (View view) {
